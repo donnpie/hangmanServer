217d01b0ff6015216ef42aaee0fc2aaa4dd85cb7
Donn <donnpie@gmail.com>
2021-04-27 15:56:20 +0200

    Initial commit

--- server.js
+++ server.js
@@ -1,22 +1,29 @@
+//Set up environment variables:
+//1. Create .env file with environment variables
+//2. Install dotenv dependency (npm i dotenv)
+//3. require('dotenv').config();
+//4. const port = process.env.PORT etc
+
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const app_id = process.env.APP_ID;
+const app_key = process.env.APP_KEY;
+console.log(port);
+
 const express = require('express');
-
 const app = express();
+app.listen(port, () => console.log(`Server started on port ${port}`));
 
-app.get('/word/:wordId', (req, res) => {
-    // const defs = ['meaning 1', 'meaning 2', 'meaning 3'];
-    // res.send(defs);
-    // const defs = [{id: 1, name: 'John'}];
-    // res.json(defs);
+app.get('/definition/:wordId', (req, res) => {
+    const wordId = req.params.wordId; 
+    //console.log('wordId', wordId);
+    
+    //First try to get root by redirect
+    const rootId = redirect('/exists/' + wordId);
+    console.log('rootId', rootId);
 
-    //Code from OD documentation
+
     const http = require("https");
-
-    const app_id = "5a3e8dec"; // insert your APP Id
-    const app_key = "6ff35dc40a6b1987baf7ce0e1f13e83d"; // insert your APP Key
-    //const wordId = "ace";
-    const wordId = req.params.wordId;
-    console.log(wordId);
-    //res.send(wordId); //test if parameter reached the server
     let result = [];
 
     const options = {
@@ -28,7 +35,6 @@ app.get('/word/:wordId', (req, res) => {
             'app_id': app_id,
             'app_key': app_key
         },
-        //mode: 'no-cors',
     };
 
     http.get(options, (resp) => {
@@ -38,25 +44,109 @@ app.get('/word/:wordId', (req, res) => {
         });
         resp.on('end', () => {
             let json = JSON.parse(body);
-            if (json.results) {
-                let senses;
-                senses = json.results[0].lexicalEntries[0].entries[0].senses;
-                senses.forEach(element => {
+           
+            if (    json.results && 
+                    json.results[0].lexicalEntries && 
+                    json.results[0].lexicalEntries[0].entries && 
+                    json.results[0].lexicalEntries[0].entries[0].senses &&
+                    json.results[0].lexicalEntries[0].entries[0].senses[0].definitions
+                ) {
+                let senses = json.results[0].lexicalEntries[0].entries[0].senses;
+                senses.forEach(sense => {
                     //console.log(element.definitions[0]);
-                    result.push(element.definitions[0]);
+                    result.push(sense.definitions[0]); 
                 });
             } else {
-                result.push("no result found - sadly the lookup only works for root words");
+                result.push("Sorry, no results found");
             }
             console.log(result);
             res.json(result);
         });
     });
-
-
 });
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.get('/exists/:wordId', (req, res) => {
+    const http = require("https");
+    const wordId = req.params.wordId; 
+    //console.log(wordId);
+    let result = [];
 
+    const options = {
+        host: 'od-api.oxforddictionaries.com',
+        port: '443',
+        path: '/api/v2/lemmas/en/' + wordId,
+        method: "GET",
+        headers: {
+            'app_id': app_id,
+            'app_key': app_key
+        },
+        redirect: 'follow'
+    };
+
+    http.get(options, (resp1) => {
+        let body1 = '';
+        resp1.on('data', (d) => {
+            body1 += d;
+        });
+        resp1.on('end', () => {
+            // let parsed = JSON.stringify(body);
+            // console.log(parsed);
+            // res.json(parsed);
+
+            let json1 = JSON.parse(body1);
+            //console.log(json);
+            //res.json(json.results[0].lexicalEntries[0].inflectionOf[0].id);
+            //res.json(json);
+            let id
+            if (    json1.results && 
+                    json1.results[0].lexicalEntries &&
+                    json1.results[0].lexicalEntries[0].inflectionOf &&
+                    json1.results[0].lexicalEntries[0].inflectionOf[0].id
+                ) {
+                id = json1.results[0].lexicalEntries[0].inflectionOf[0].id;
+            } else {
+                id = wordId;
+            }
+            console.log(id);
+
+            //Insert second http request here
+            options.path = '/api/v2/entries/en-gb/' + id;
+
+            http.get(options, (resp2) => {
+                let body2 = '';
+                resp2.on('data', (d) => {
+                    body2 += d;
+                });
+                resp2.on('end', () => {
+                    let json2 = JSON.parse(body2);
+                   
+                    if (    json2.results && 
+                            json2.results[0].lexicalEntries && 
+                            json2.results[0].lexicalEntries[0].entries && 
+                            json2.results[0].lexicalEntries[0].entries[0].senses &&
+                            json2.results[0].lexicalEntries[0].entries[0].senses[0].definitions
+                        ) {
+                        let senses = json2.results[0].lexicalEntries[0].entries[0].senses;
+                        senses.forEach(sense => {
+                            //console.log(element.definitions[0]);
+                            result.push(sense.definitions[0]); 
+                        });
+                    } else {
+                        result.push("Sorry, no results found");
+                    }
+                    console.log(result);
+                    res.json(result);
+                });
+            });
+
+
+            //res.json(id);
+        });
+    });
+});
+
+//Web resources:
+//https://www.youtube.com/watch?v=v0t42xBIYIs&t=1048s //Building react/express app - Traversy media
+//https://www.youtube.com/watch?v=v0t42xBIYIs&t=1048s //Using dotenv with NodeJS and Environment Variables - Steve Griffith
+//https://codewithmosh.com/courses/293204/lectures/4509494 //Route parameters - CodewithMosh
 
